b5f207f0a029538f4d5b9bb6b82f1dce24583972
Abu Saleh Md Siam <abusalehmdsiam@gmail.com>
2025-01-12 04:32:29 +0600

    user collection get,post,delete,patch

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ async function run() {
     const menuCollections = client.db("bistroDb").collection("menu");
     const reviewCollections = client.db("bistroDb").collection("reviews");
     const cartCollections = client.db("bistroDb").collection("carts");
+    const usersCollections = client.db("bistroDb").collection("users");
+
+    // menu Collections
     app.get("/menu", async (req, res) => {
       const result = await menuCollections.find().toArray();
       res.send(result);
@@ -54,6 +57,44 @@ async function run() {
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // user collections
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollections.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollections.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist" });
+      }
+      const result = usersCollections.insertOne(user);
       res.send(result);
     });
 
